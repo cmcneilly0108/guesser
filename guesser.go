b58e95e754405742b0797a0ea53c453aa7b38c18
Chris McNeilly <cmcneilly@motistainsights.com>
2013-06-04 15:11:08 -0700

    fixed agd

--- guesser.go
+++ guesser.go
@@ -22,8 +22,6 @@ func generate(len int) string {
 }
 
 func evaluate(guess string, answer string) (int, int, int) {
-	/* TBD - try passing in a pointer to a struct and change it 
-	   instead of passing back 3 integers */ 
 	exact, less, greater := 0, 0, 0
 	lofa := len(answer)
 	g, a := "", ""
@@ -63,6 +61,34 @@ func incGuess (g string, pos int) string {
 	return res
 }
 
+func decGuess (g string, pos int) string {
+	res := g[0:pos]
+	nnum, err := strconv.Atoi(g[pos:pos+1])
+	if err != nil {
+		fmt.Println(err)
+        }
+	nnum--
+	res += strconv.Itoa(nnum)
+	res += g[pos+1:len(g)]
+	return res
+}
+
+func inc2Guess (g string, pos int) string {
+	res := g[0:pos]
+	nnum, err := strconv.Atoi(g[pos:pos+1])
+	if err != nil {
+		fmt.Println(err)
+        }
+        if (nnum < 8) {
+        	nnum = nnum + 2
+        } else {
+        	nnum++
+        }
+	res += strconv.Itoa(nnum)
+	res += g[pos+1:len(g)]
+	return res
+}
+
 func bruteForce(answer string) int {
 	/* This only ever uses the exact data from the evaluate function.
 	   We can do better. */
@@ -94,6 +120,43 @@ func bruteForce(answer string) int {
 	return guesses
 }
 
+func skipOne(answer string) int {
+	guesses, lofa := 0, len(answer)
+	cur := 0
+	previous := guess{genAll(lofa,"0"), 0,0,0}
+	previous.exact, previous.less, previous.greater = evaluate(previous.gstring,answer)
+	guesses++
+	current := guess{"",0,0,0}
+
+	for cur < lofa { 
+		current = guess{inc2Guess(previous.gstring,cur),0,0,0}
+		current.exact, current.less, current.greater = evaluate(current.gstring,answer)
+		//fmt.Print(current)
+		guesses++
+		//fmt.Print(" guess #")
+		//fmt.Println(guesses)
+		if (current.exact > previous.exact) {
+			cur++
+			previous = current
+		} else {
+			if (current.exact < previous.exact) {
+				cur++
+			} else {
+				if (current.greater > previous.greater) {
+					previous = guess{decGuess(current.gstring,cur),0,0,0}
+					previous.exact, previous.less, previous.greater = evaluate(previous.gstring,answer)
+					cur++
+				} else {
+					previous = current
+				}
+			}
+		}
+	}
+	//fmt.Print("The correct answer is =")
+	//fmt.Println(previous)
+	return guesses
+}
+
 // TBD - skip1 - inc2Guess 
 // TBD - bothEnds - decGuess
 // TBD - combine both above - is possible?
@@ -103,8 +166,9 @@ func bruteForce(answer string) int {
 func main() {
 	rand.Seed(time.Now().Unix())
 	const tests = 1000
-	const numSize = 200
+	numSize := 20
 	total := 0
+	var agd float64
 	var scores [tests] int
 
 	fmt.Println("Brute force")
@@ -115,7 +179,22 @@ func main() {
 		//fmt.Println("It took "+strconv.Itoa(scores[j]) + " guesses")
 		total += scores[j]
 	}
-	fmt.Print("Average guesses = ")
-	fmt.Println(total/tests)
+	fmt.Print("Average guesses/digit = ")
+	agd = float64(total)/(float64(tests)*float64(numSize))
+	fmt.Println(agd)
+
+	fmt.Println("Skip One")
+	total = 0
+	for j:=0;j<tests;j++ {
+		answer := generate(numSize)
+		//fmt.Println("The answer is = "+answer)
+		scores[j] = skipOne(answer)
+		//fmt.Println("It took "+strconv.Itoa(scores[j]) + " guesses")
+		total += scores[j]
+	}
+	fmt.Print("Average guesses/digit = ")
+	agd = float64(total)/(float64(tests)*float64(numSize))
+	fmt.Println(agd)
+
 }
 
