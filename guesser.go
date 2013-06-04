6e51021c8b4a1b9aeeb77b0fd15d6effb9738896
Chris McNeilly <cmcneilly0108@gmail.com>
2013-06-03 22:13:00 -0700

    incGuess in progress

--- guesser.go
+++ guesser.go
@@ -6,6 +6,13 @@ import ("fmt"
 	"time"
 	)
 
+type guess struct {
+	gstring string
+	exact int
+	less int
+	greater int
+}
+
 func generate(len int) string {
 	result := ""
 	for i:=1;i<= len;i++ {
@@ -34,19 +41,48 @@ func evaluate(guess string, answer string) (int, int, int) {
 	return exact, less, greater
 }
 
+func genAll(length int, char string) string {
+	result := ""
+	for i:=0;i<length;i++ {
+		result += char
+	}
+	return result
+}
+
+func incGuess (g string, pos int) string {
+	res := g[0:5]
+	//int nnum
+	nnum := strconv.Atoi(g[5:6])
+	nnum++
+	res += strconv.Itoa(nnum)
+	res += g[6:len(g)]
+	return res
+}
+
 func bruteForce(answer string) int {
 	guesses, lofa := 0, len(answer)
+	cur := 0
+	previous := guess{genAll(lofa,"0"), 0,0,0}
+	previous.exact, previous.less, previous.greater = evaluate(previous.gstring,answer)
+	guesses++
+	current := guess{incGuess(previous.gstring,cur),0,0,0}
 
-	guess := generate(lofa)
-	evaluate(guess,answer)
-	fmt.Println(guess)
+	/* gen++, evaluate, then compare results
+	   if exact, cur++ and gen++, if lower, last guess, cur++ and g++
+	   else gen++
+	for cur < lofa {
+		
+	} */
+	   	   
+	fmt.Println(previous)
+	fmt.Println(current)
 	return guesses
 }
 
 func main() {
 	rand.Seed(time.Now().Unix())
 	answer := generate(20)
-	fmt.Println("The answer is ="+answer)
+	fmt.Println("The answer is = "+answer)
 
 	fmt.Println("Brute force")
 	guesses := bruteForce(answer)
